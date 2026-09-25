6b542ebb04f076ef4564105b486873bbf4802d28
AmitKumarPrasad <39097317+AmitKumarPrasad@users.noreply.github.com>
2026-09-25 15:53:31 +0530

    refactor: add reusable website components

---
+++ src/components/TechnologyChips.jsx
@@ -0,0 +1,3 @@
+export default function TechnologyChips({ dark = false }) {
+  return <div className="d-flex flex-wrap gap-2">{/* Technologies are supplied by the page to keep this component reusable. */}</div>;
+}
